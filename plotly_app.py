5ec8b8258eed4b416638b0a1838df6af5b9b4e52
Kostya Deyev <kdeyev@gmail.com>
2017-08-06 09:16:11 +0300

    plotly_app experiments

--- plotly_app.py
+++ plotly_app.py
@@ -4,165 +4,15 @@
 
 import remi.gui as gui
 from remi import start, App
-import json
 
 import stackData
+import plotly_app_plot
 
-
-
-class PlotlyWidget(gui.Widget):
-    def __init__(self, **kwargs):
-        
-        self._id = str(id (self))
-        super(PlotlyWidget, self).__init__(id=self._id, **kwargs)
-        
-        self._data = stackData.data
- 
-        javascript_code = gui.Tag()
-        javascript_code.type = 'script'
-        javascript_code.attributes['type'] = 'text/javascript'
-		
-        code = """
-            var PLOT = document.getElementById('%(id)s');
-            var zmax = 20000;
-            var config = {
-    			modeBarButtonsToRemove : ["sendDataToCloud", ],
-    			displaylogo: false,
-    			displayModeBar: true,
-    			//scrollZoom: true,
-    			//editable: true,
-    		};
-
-          var layout = {
-    				title: 'very first version of Wiggle',
-				xaxis: {title: 'CMP'},
-    			  yaxis: {title: 'Time', autorange: 'reversed',},
-    			  showlegend: false,
-    			};
-    
-    		Plotly.newPlot(PLOT, [], layout, config);
-      
-              var drawWiggle = function (PLOT, twoDArray) {
-                    var data = [];
-    			
-    			var step = 1;
-    			var gain = 1;
-    			for (var trace=0;trace<twoDArray.length;trace+=step) {
-    				var x = new Array (twoDArray[trace].length);
-    				var y = new Array (twoDArray[trace].length);
-    				var ampl = new Array (twoDArray[trace].length);
-    				var fill_ampl = new Array (twoDArray[trace].length);
-    				
-    				
-    				for (var i=0;i<twoDArray[trace].length;i++) {
-    					x[i] = trace;
-    					y[i] = i;
-    					ampl[i] = trace + twoDArray[trace][i]/zmax*step*gain;
-    					if (twoDArray[trace][i] > 0) {
-    						fill_ampl[i] = ampl[i]
-    					} else {
-    						fill_ampl[i] = trace;
-    					}
-    					
-    				}
-    				
-    				var zero_line = {
-    				  x: x,
-    				  y: y,
-    				  type: 'scatter',
-    				  line: {color: 'black',
-    						width: 0,
-    						},
-    				};
-    
-    				var fill = {
-    				  x: fill_ampl,
-    				  y: y,
-    				  type: 'scatter',
-    				  fill: 'tonextx',
-    				  line: {color: 'black',
-    						width: 0,
-    						//shape: 'spline',
-    						},
-    				};
-    
-    				var wiggle = {
-    				  x: ampl,
-    				  y: y,
-    				  type: 'scatter',
-    				  line: {color: 'black',
-    						width: 1,
-    						//shape: 'spline',
-    						},
-    				};
-    
-    				data.push(zero_line);
-    				data.push(fill);
-    				data.push(wiggle);
-    			}
-    			var layout = {
-    				title: 'very first version of Wiggle',
-    				xaxis: {title: 'CMP'},
-    			  yaxis: {title: 'Time', autorange: 'reversed',},
-    			  showlegend: false,
-    			};
-    
-    			Plotly.newPlot(PLOT, data, layout, config);
-               };
-               
-               var drawMatrix = function (PLOT, twoDArray) {
-                   	var stack = 
-			  {
-				z: twoDArray,
-				type: 'heatmap',
-				transpose : true,
-				zsmooth : 'best',
-				zauto : false,
-				zmin : -zmax,
-				zmax : zmax,
-				colorscale : [[0, 'rgb(0,0,255)'], [0.5, 'rgb(255,255,255)'], [1, 'rgb(255,0,0)']],
-				//colorbar : {symmetric: true,}
-			};
-   
-    			var layout = {
-    				title: 'very first version of Wiggle',
-    				xaxis: {title: 'CMP'},
-    			  yaxis: {title: 'Time', autorange: 'reversed',},
-    			  showlegend: false,
-    			};
-    
-    			Plotly.newPlot(PLOT, [stack], layout, config);
-               };
-               
-               
-      
-          var updateData = function (id) {      
-    		var PLOT = document.getElementById(id);
-    		var url = id + "/get_refresh";
-    		Plotly.d3.json(url,
-    		function(error, twoDArray) {
-    			drawMatrix (PLOT, twoDArray );
-    		});
-            };
-            """% {'id': self._id}
-		
-        javascript_code.add_child('code',   # Add to Tag
-                                  code % {'id': id(self), })
-        self.add_child('javascript_code', javascript_code)   # Add to widget
-
-    def get_refresh(self):
-        if self._data is None:
-            return None, None
-
-        txt = json.dumps(self._data)
-        headers = {'Content-type': 'text/plain'}
-        return [txt, headers]
 
 class MyApp(App):
     def __init__(self, *args):
         html_head = '<script src="https://cdn.plot.ly/plotly-latest.min.js">'
         html_head += '</script>'
-        self._data = stackData.data
         super(MyApp, self).__init__(*args, html_head=html_head)
 
     def main(self):
@@ -173,11 +23,19 @@ class MyApp(App):
 
         plotContainer = gui.Widget()
 
-        self.plt1 = PlotlyWidget()
-        plotContainer.append(self.plt1)
+        self._plotsWidget = plotly_app_plot.PlotsWidget(
+            self, width='100%', height='100%')
+        
+        centralContainer = gui.VBox()
+        plotContainer.append(centralContainer)
+        centralContainer.append(self._plotsWidget)
 
-        self.plt2 = PlotlyWidget()
-        plotContainer.append(self.plt2)
+        self.plt1 = self._plotsWidget.createPlot('1')
+#        centralContainer.append(self.plt1)
+
+#        self.plt2 = PlotlyWidget()
+        self.plt2 = self._plotsWidget.createPlot('2')
+#        centralContainer.append(self.plt2)
         
         bt1 = gui.Button('Start', width=200, height=30)
         bt1.style['margin'] = 'auto 50px'
@@ -203,16 +61,10 @@ class MyApp(App):
 
     # listener function
     def on_button_pressed1(self, widget):        
-        cmd = """
-            updateData('%(id)s');
-        """%{'id' : self.plt1._id}
-        self.execute_javascript(cmd)
+        self.plt1.setData (stackData.data, None)
         
     def on_button_pressed2(self, widget):        
-        cmd = """
-            updateData('%(id)s');
-        """%{'id' : self.plt2._id}
-        self.execute_javascript(cmd)
+        self.plt2.setData (stackData.data, None)
 
 if __name__ == "__main__":
     # starts the webserver
