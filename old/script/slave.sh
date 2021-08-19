297774bae2a68e80d8f51c210da204c67a76b252
Kostya Deev <kostya.deev@bluware.com>
2021-08-19 23:26:54 +0000

    remove old code

--- old/script/slave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install python-numpy -y
-sudo apt-get install python-matplotlib -y
-sudo gsutil cp gs://kdeyev_europe_west1/cwp.tar.gz /usr/local/bin/
-cd /usr/local/bin
-sudo tar xvf cwp.tar.gz 
