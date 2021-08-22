4939ddaee290be15c9d4a62f982700dd63986f8d
Kostya Deev <kostya.deev@bluware.com>
2021-08-22 06:59:59 +0000

    add cors

--- src/suspark_service/main.py
+++ src/suspark_service/main.py
@@ -11,12 +11,16 @@
 
 import configargparse
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 from suspark.pipeline_repository import PiplineRepository
 from suspark.suspark_context import SusparkContext
 from suspark.suspark_modules_factory import ModulesFactory
 from suspark_modules.suspark_test_modules import register_test_modules
 from suspark_service.suspark_service_app import create_suspark_service_app
+
+# from fastapi.staticfiles import StaticFiles
+
 
 defaults = {"port": 9091}
 
@@ -35,6 +39,15 @@ suspark_context = SusparkContext()
 pipeline_repository = PiplineRepository(suspark_context=suspark_context, modules_factory=modules_factory)
 
 app = create_suspark_service_app(modules_factory=modules_factory, pipeline_repository=pipeline_repository)
+
+# app.mount("/", StaticFiles(directory="src/ui/dist", html=True), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if options.allow_remote:
     host = "0.0.0.0"
