a51e6816e6e82aadb79624b9943ddff10caa8a9a
Kostya Deev <kostya.deev@bluware.com>
2021-08-29 05:17:10 +0000

    deliver python code

--- src/suspark/suspark_context.py
+++ src/suspark/suspark_context.py
@@ -1,14 +1,30 @@
 import os
-import shutil
+from zipfile import ZipFile
 
 import pyspark
 from pyspark.sql import SparkSession
 
 
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, "..")))
+
+
 class SusparkContext:
     def __init__(self) -> None:
-        # seisspark_home = os.environ["SEISSPARK_HOME"]
-        # shutil.make_archive("seisspark", "zip", "src")
+        seisspark_home = os.environ["SEISSPARK_HOME"]
+        seisspark_zip_pile = "seisspark.zip"
+
+        if os.path.exists(seisspark_zip_pile):
+            os.remove(seisspark_zip_pile)
+
+        with ZipFile(seisspark_zip_pile, mode="a") as myzipfile:
+            zipdir(f"{seisspark_home}/src/su_data", myzipfile)
+            zipdir(f"{seisspark_home}/src/su_rdd", myzipfile)
+            zipdir(f"{seisspark_home}/src/suspark", myzipfile)
+            zipdir(f"{seisspark_home}/src/suspark_modules", myzipfile)
 
         spark_conf = pyspark.SparkConf()
         if "SPARK_MASTER_URL" in os.environ:
@@ -29,7 +45,7 @@ class SusparkContext:
         spark_sess.read
         spark_sess.readStream
         spark_ctxt.setLogLevel("WARN")
-        spark_ctxt.addPyFile("seisspark.zip")
+        spark_ctxt.addPyFile(seisspark_zip_pile)
 
         self._spark_ctxt = spark_ctxt
 
